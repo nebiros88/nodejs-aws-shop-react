989fc8ca189f7ecb3ff1842374d901613e92da67
nebiros88 <nebiros1988@gmail.com>
2026-06-12 17:28:59 +0200

    task-8: integrate cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: 'https://h8j8j6pg2c.execute-api.eu-central-1.amazonaws.com',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://cdxn9bcxyk.execute-api.eu-central-1.amazonaws.com',
   import: 'https://h8j8j6pg2c.execute-api.eu-central-1.amazonaws.com',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://cdxn9bcxyk.execute-api.eu-central-1.amazonaws.com/api',
 };
 
 export default API_PATHS;
