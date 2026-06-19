c3be88a0636d42c379b216627e674f07a6bcedd9
nebiros88 <nebiros1988@gmail.com>
2026-06-19 17:43:48 +0200

    task-9: adjust cart and orders api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,10 @@
 const API_PATHS = {
   product: 'https://h8j8j6pg2c.execute-api.eu-central-1.amazonaws.com',
-  order: 'https://cdxn9bcxyk.execute-api.eu-central-1.amazonaws.com',
+  order:
+    'http://nebiros88-cart-api-develop.eu-central-1.elasticbeanstalk.com/api',
   import: 'https://h8j8j6pg2c.execute-api.eu-central-1.amazonaws.com',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://cdxn9bcxyk.execute-api.eu-central-1.amazonaws.com/api',
+  cart: 'http://nebiros88-cart-api-develop.eu-central-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
