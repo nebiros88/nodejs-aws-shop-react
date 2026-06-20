995e333407330a17a4bf303bc8c460c2d62cf8f2
nebiros88 <nebiros1988@gmail.com>
2026-06-20 18:41:30 +0200

    task-9: configure distribution to redirect cart-api requests to eb env

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
 const API_PATHS = {
   product: 'https://h8j8j6pg2c.execute-api.eu-central-1.amazonaws.com',
-  order:
-    'http://nebiros88-cart-api-develop.eu-central-1.elasticbeanstalk.com/api',
+  order: 'https://d2jt7nau891b0d.cloudfront.net/api',
   import: 'https://h8j8j6pg2c.execute-api.eu-central-1.amazonaws.com',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://nebiros88-cart-api-develop.eu-central-1.elasticbeanstalk.com/api',
+  cart: 'https://d2jt7nau891b0d.cloudfront.net/api',
 };
 
 export default API_PATHS;
